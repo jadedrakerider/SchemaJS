5334f4b2b3053c73a27497dba4dcf4eeea1da93b
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-11-15 21:19:06 -0700

    set up tests for schema

--- test/Schema.chai.mjs
+++ test/Schema.chai.mjs
@@ -2,26 +2,36 @@ import { Schema, array, boolean, integer, number, nulled, object, string } from 
 import { Assertion, expect } from 'chai'
 
 let counter = 1;
+const o = {
+    arr: ['str1', 'str2'],
+    bool: true,
+    integer: 5,
+    number: 1.0,
+    nulled: null,
+    str: 'string',
+    subobject: {
+        subarr: [],
+        subbool: false,
+        subint: -2,
+        sibnum: -3.14,
+        nulled: null,
+        str: 'another'
+    }
+};
 
 describe('Schema', () => {
     it('Schema Constructor', () => {
         it(`Test ${counter}: Types`, () => {
-            const o = {
-                arr: ['str1', 'str2'],
-                bool: true,
-                integer: 5,
-                number: 1.0,
-                nulled: null,
-                str: 'string',
-            }
-
             const schema = new Schema();
             schema.add('arr', array);
             schema.add('bool', boolean);
             schema.add('integer', integer);
             schema.add('number', number);
             schema.add('nulled', nulled);
-            schema.add('str', string)
+            schema.add('str', string);
+            
+
+
             
 
         });
