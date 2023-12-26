c8c9d43c672a79ee9d6c1bbbe8dcb94c0dddca9d
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-12-26 13:17:58 -0700

    Refactored to private fields

--- test/Schema.chai.mjs
+++ test/Schema.chai.mjs
@@ -1,12 +1,11 @@
-import { Schema, 
-         SchemaType,
-         array,
-         boolean,
-         integer,
-         number,
-         nulled,
-         object,
-         string } from '../Schema.mjs'
+import { Schema,
+         ArrayType,
+         BooleanType,
+         IntegerType,
+         NulledType,
+         NumberType,
+         ObjectType,
+         StringType } from '../Schema.mjs'
 import { expect, assert } from 'chai'
 import Ajv from "ajv";
 
@@ -27,32 +26,32 @@ const o = {
 };
 
 const unit = {
-    'id': number,
-    'state': string,
-    'zip': string,
-    'group': object,
-    'country': string,
-    'group': object,
-    'city': string,
-    'timezone': string,
-    'marketing_name': string,
-    'street_address_1': string,
-    'street_address_2': string,
-    'unit_code': string,
-    'hub': object,
-    'hub_id': number,
-    'group_id': number,
-    'parking_enabled': boolean,
-    'temperature_scale': string,
-    'has_hub': boolean,
-    'broadcast_messages_enabled': boolean,
-    'service_requests_enabled': boolean,
-    'ring_enabled': boolean,
-    'security_system_enabled': boolean,
-    'livly_enabled': boolean,
-    'portal_only': boolean,
-    'urgent_request_custom_copy': string,
-    'urgent_request': boolean
+    'id': Schema.number,
+    'state': Schema.string,
+    'zip': Schema.string,
+    'group': Schema.object,
+    'country': Schema.string,
+    'group': Schema.object,
+    'city': Schema.string,
+    'timezone': Schema.string,
+    'marketing_name': Schema.string,
+    'street_address_1': Schema.string,
+    'street_address_2': Schema.string,
+    'unit_code': Schema.string,
+    'hub': Schema.object,
+    'hub_id': Schema.number,
+    'group_id': Schema.number,
+    'parking_enabled': Schema.boolean,
+    'temperature_scale': Schema.string,
+    'has_hub': Schema.boolean,
+    'broadcast_messages_enabled': Schema.boolean,
+    'service_requests_enabled': Schema.boolean,
+    'ring_enabled': Schema.boolean,
+    'security_system_enabled': Schema.boolean,
+    'livly_enabled': Schema.boolean,
+    'portal_only': Schema.boolean,
+    'urgent_request_custom_copy': Schema.string,
+    'urgent_request': Schema.boolean
 }
 
 const ajv = new Ajv()
@@ -65,14 +64,15 @@ let counter = 1;
 
 describe('Schema mjs', () => {
     describe('SchemaType', () => {
-        it(`Test ${counter}: SchemaType constructor`, () => {    
-            expect(string.keyValueOf()).to.eql({STRING: {'type':'string'}})
-            expect(array.keyValueOf()).to.not.eql({STRING: {'type':'string'}})
+        it(`Test ${counter}: SchemaType constructor`, () => {
+            const schema = new Schema()
+            expect(Schema.string.keyValueOf()).to.eql({STRING: {'type':'string'}})
+            expect(Schema.array.keyValueOf()).to.not.eql({STRING: {'type':'string'}})
         })
         counter++;
 
         it(`Test ${counter}: SchemaType.toString()`, () => {
-            expect(string.toString()).to.eql('{"type":"string"}')
+            expect(Schema.string.toString()).to.eql('{"type":"string"}')
         })
         counter++;
     })
@@ -80,19 +80,19 @@ describe('Schema mjs', () => {
     describe('Schema Constructor', () => {
         it(`Test ${counter}: toString`, () => {
             const schema = new Schema()
-            schema.add('token', string)
-            schema.add('Jenny', number)
+            schema.add('token', Schema.string)
+            schema.add('Jenny', Schema.number)
         })
         counter++;
 
         it(`Test ${counter}: Recognizes Types`, () => {
             const schema = new Schema();
-            schema.add('arr', array);
-            schema.add('bool', boolean);
-            schema.add('number', number);
-            schema.add('nulled', nulled);
-            schema.add('str', string);
-            schema.add('subobject', object);
+            schema.add('arr', Schema.array);
+            schema.add('bool', Schema.boolean);
+            schema.add('number', Schema.number);
+            schema.add('nulled', Schema.nulled);
+            schema.add('str', Schema.string);
+            schema.add('subobject', Schema.object);
 
             const validate = ajv.compile(schema);
             const result = validate(o);
